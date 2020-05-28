2e05c987f16f39844245f0b6917058d25f8f6944
Stephen Chung <spchung95@gmail.com>
2020-05-27 21:40:05 -0700

    display list of timeline events

--- db.py
+++ db.py
@@ -30,15 +30,29 @@ def create_timeline_event(cur, event):
     end = event.get('end', '')
     query = "INSERT INTO timeline_events (title, description, start, end) VALUES (?, ?, ?, ?)"
     res = cur.execute(query, (title, description, start, end))
+
     return res.lastrowid
+
+def get_all_timeline_events(cur):
+    query = "SELECT * FROM timeline_events"
+    res = cur.execute(query)
+    for e in res:
+        print(e)
+
+def drop_table(cur, table_name):
+    query = "DROP TABLE {}".format(table_name)
+    cur.execute(query)
 
 
 if __name__ == "__main__":
     conn = sqlite3.connect('timeline.db')
 
     cur = conn.cursor()
-    cur.execute("DROP TABLE timeline_events")
+    # drop_table(cur, 'timeline_events')
     cur.execute(CREATE_QUERY)
     create_timeline_event(cur, FAKE_TIMELINE[0])
     conn.commit()
+
+    get_all_timeline_events(cur)
+
     conn.close()
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return send_from_directory('.', 'index.html')
 
 if __name__ == "__main__":
+    print('hello')
+    app.run()
