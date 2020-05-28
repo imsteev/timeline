383dec8e7c37143b52a46f507e108ede88123db0
Stephen Chung <spchung95@gmail.com>
2020-05-27 22:16:35 -0700

    favicon

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, send_from_directory
 
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static/')
 
 @app.route('/')
 def index():
