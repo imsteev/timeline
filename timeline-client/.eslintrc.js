5b67231b419214b3e2c4a76457c7316c403f68c2
Stephen Chung <spchung95@gmail.com>
2020-07-18 10:50:06 -0700

    add eslint

---
+++ timeline-client/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": "plugin:react/recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
